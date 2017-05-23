e24406f95da9e4301f2062d00bc60448baa524f9
hmrigg17 <hmrigg17@transy.edu>
2017-05-23 00:25:28 -0400

    Added Pause Menu(Not working)

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+	public GameObject PauseUI;
+
+	private bool paused = false;
+
+	void Start (){
+		PauseUI.SetActive (false);
+	}
+
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.Escape)) {
+			paused = !paused;
+		}
+
+		if (paused) {
+			PauseUI.SetActive (true);
+			Time.timeScale = 0;
+		} 
+		else {
+			PauseUI.SetActive (false);
+			Time.timeScale = 1;
+		}
+	}
+
+}
+
+
+
